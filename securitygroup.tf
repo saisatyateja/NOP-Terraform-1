7c45e6e76674a5a35283b09b40cb17ac5d8b7af4
Aman Saad <amansaad7720@gmail.com>
2023-07-12 18:55:07 +0530

    route and sg added

--- securitygroup.tf
+++ securitygroup.tf
@@ -15,9 +15,9 @@ resource "aws_security_group" "mysg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
-        from_port   = -1
-        to_port     = -1
-        protocol    = "tcp"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
   depends_on= [aws_vpc.main]
