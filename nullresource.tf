808abb4308378344c8db541ae8e01b91b19bfdad
Aman Saad <amansaad7720@gmail.com>
2023-07-12 17:46:26 +0530

    route and sg added

--- nullresource.tf
+++ nullresource.tf
@@ -7,14 +7,14 @@ resource "null_resource" "remote"{
 }
 provisioner "remote-exec" {
          inline = [
-                      "sudo apt update",
-                      "sudo apt install dotnet-sdk-7.0 -y",
-                      "git clone https://github.com/4mmnnn/nopCommerceJuly23.git /home/ubuntu",
-                      "cd /home/ubuntu/nopCommerceJuly23/",
-                      "dotnet build src/NopCommerce.sln",
-                      "cd src/Presentation/Nop.Web",
-                      "dotnet publish Nop.Web.csproj -c Release -o /home/ubuntu/nop",
-                      "dotnet /home/ubuntu/nop/Nop.Web.dll --URLS='http://0.0.0.0:5000'"
+                    "sudo apt-get update",
+                    "sudo apt install dotnet-sdk-7.0 -y",
+                    "git clone https://github.com/4mmnnn/nopCommerceJuly23.git",
+                    "cd nopCommerceJuly23/",
+                    "dotnet build src/NopCommerce.sln",
+                    "cd src/Presentation/Nop.Web",
+                    "dotnet publish Nop.Web.csproj -c Release -o /home/ubuntu/nop",
+                    "dotnet /home/ubuntu/nop/Nop.Web.dll --URLS='http://0.0.0.0:5000'"
                   ]
   }
   }
